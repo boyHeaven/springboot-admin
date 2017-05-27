7f46ff8f127b6ff9bb4796b047b47c09546b31ce
杨学斌 <18103345886@163.com>
2017-05-27 14:45:37 +0800

    add slf4j log

--- springboot-service/src/main/java/cn/com/yxb/springboot/user/impl/UserServiceImpl.java
+++ springboot-service/src/main/java/cn/com/yxb/springboot/user/impl/UserServiceImpl.java
@@ -3,10 +3,14 @@ package cn.com.yxb.springboot.user.impl;
 import cn.com.yxb.springboot.bean.UserBean;
 import cn.com.yxb.springboot.mapper1.UserDao;
 import cn.com.yxb.springboot.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created on 2017/5/24.
@@ -17,11 +21,19 @@ import javax.annotation.Resource;
 @Transactional
 public class UserServiceImpl implements UserService {
 
+    Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Resource
     private UserDao userDao;
 
     @Override
     public boolean userLogin(String loginName, String password) throws Exception {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm");
+
+        logger.info("enter login controller date --> [{}] name --> [{}]  password ---> [{}]",
+                simpleDateFormat.format(new Date()), loginName, password);
+
         UserBean userBean = userDao.findOneByUserLoginName(loginName, password);
         if (userBean != null) {
             return true;
--- springboot-web/src/main/java/cn/com/yxb/springboot/controller/UserController.java
+++ springboot-web/src/main/java/cn/com/yxb/springboot/controller/UserController.java
@@ -1,11 +1,16 @@
 package cn.com.yxb.springboot.controller;
 
 import cn.com.yxb.springboot.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created on 2017/5/24.
@@ -16,12 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserController {
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String userLogin(@RequestParam(value = "name") String name,
                             @RequestParam(value = "password") String password) {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm");
+        logger.info("enter login controller date --> [{}] name --> [{}]  password ---> [{}]",
+                simpleDateFormat.format(new Date()), name, password);
 
         System.out.println("++++++++++++++name: " + name);
         System.out.println("++++++++++++++password: " + password);
